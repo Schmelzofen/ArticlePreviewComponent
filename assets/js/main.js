fdd1589b6648cd4ea07f22a1af21274fce564bd0
Sertac Özdemir <sertac.oezdemir@gmail.com>
2022-01-15 12:19:15 +0100

    Desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 let button = document.getElementById("checkbox1")
 let firstContainer = document.getElementById("firstContainer")
 let secondContainer = document.getElementById("secondContainer")
-
+let desktopContainer = document.getElementById("socialMedia")
 button.addEventListener("click", function () {
     secondContainer.classList.toggle("hide")
     firstContainer.classList.toggle("show")
+    desktopContainer.classList.toggle("hide")
 })
